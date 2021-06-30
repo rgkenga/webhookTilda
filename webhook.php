1d32da316848fc08264834a7e120f79c06bafc27
rgkenga <86708849+rgkenga@users.noreply.github.com>
2021-06-30 20:15:49 +0500

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -3,5 +3,5 @@ header('Access-Control-Allow-Origin: *');
 $headers = "From: rgkenga@yandex.ru";
 $message = print_r($_POST,true);
 @mail('rgkenga@yandex.ru', 'Tilda TEST', $message, $headers);
-echo "ok";
+echo "test";
 ?>
